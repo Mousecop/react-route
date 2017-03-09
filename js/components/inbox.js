443dbce785ae09c89fb261d67ff4928e1f69d87d
Mousecop <joaskew13@gmail.com>
2017-03-09 12:13:33 -0500

    add root

---
+++ js/components/inbox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Inbox (props) {
+    return (
+        <div className="inbox">
+            {props.displayInbox}
+        </div>
+    );
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import Sidebar from 'js/components/sidebar';
+import Inbox from './inbox';
+import Spam from './spam';
 
+const routes =(
+    <Router history={hashHistory}>
+        <Route path="/" component={Sidebar}>
+            <IndexRoute component={ContactListContainer} />
+            <Route path="/inbox" component={Inbox} />
+        </Route>
+    </Router>
+);
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<routes />, document.getElementById('root'))
+);
